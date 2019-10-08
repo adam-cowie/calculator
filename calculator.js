9e391678947570f9b90a8e465b54a292e315317d
adam cowie <cowie.adamj@gmail.com>
2019-10-09 12:33:15 +1300

    add-pseudocode

---
+++ calculator.js
@@ -0,0 +1,42 @@
+// arr var to store entries
+// var total to store answers
+// var temp and set to empty '.'
+
+// make button to run function
+// run if function for value isNan or equal to .'.
+// temp += val 
+// get html element answer/input box and make #answer extract from the temp string from 0 - 10
+
+
+// else if AC to clear everything
+// else if CE to clear last entry only
+
+// else if function for multiplying
+// add current number to entries
+// add multiplication symbol to entries
+// clear temp
+
+// else if function for dividing
+// add current number to entries
+// add division symbol to entries
+// clear temp
+
+// else if equals sign
+// push the value in temp into entries
+// create var nt and store amount of entries starting at 0
+// loop through entries 
+// create var nextNum (the next item in entries)
+// var symbol = entries[i]
+// create if function, if symbol is +, function nt += nextNum
+// create else if function, if symbol is -, function nt -= nextNum
+// create else if function, if symbol is *, function nt *= nextNum
+// create else if function, if symbol is /, function nt /= nextNum
+
+ 
+// show negative answers
+// Swap the '-' symbol so text input handles it correctly
+// if nt is less than 0, Math.abs(nt) + '-';
+
+// answer = value of nt
+// clear entries, clear temp
+// else statement, add temp and val to entries and clear.
