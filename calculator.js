e99092dfdd500d50f154f2db7e2d0ed0cf09e423
adam cowie <cowie.adamj@gmail.com>
2019-10-09 13:02:46 +1300

    add-calculator-frist-draft

--- calculator.js
+++ calculator.js
@@ -40,3 +40,65 @@
 // answer = value of nt
 // clear entries, clear temp
 // else statement, add temp and val to entries and clear.
+
+var entries = [];
+var total = 0;
+
+var temp = '';
+$("button").on('click', function() {
+ 	var val = $(this).text();
+
+  if (!isNaN(val) || val === '.') {
+    temp += val;
+    $("#answer").val(temp.substring(0,10));
+
+  } else if (val === 'AC') {
+    entries = [];
+    temp = '';
+    total = 0;
+    $("#answer").val('')
+
+  } else if (val === 'CE') {
+    temp = '';
+    $("#answer").val('')
+
+  } else if (val === 'x') {
+    entries.push(temp);
+    entries.push('*');
+    temp = '';
+
+  } else if (val === '÷') {
+    entries.push(temp);
+    entries.push('/');
+    temp = '';
+
+  } else if (val === '=') {
+  	entries.push(temp);
+
+    var nt = Number(entries[0]);
+    for (var i = 1; i < entries.length; i++) {
+      var nextNum = Number(entries[i+1])
+      var symbol = entries[i];
+      
+      if (symbol === '+') { nt += nextNum; } 
+      else if (symbol === '-') { nt -= nextNum; } 
+      else if (symbol === '*') { nt *= nextNum; } 
+      else if (symbol === '/') { nt /= nextNum; }
+      
+      i++;
+    }
+
+    if (nt < 0) {
+      nt = Math.abs(nt) + '-';
+    }
+    
+    $("#answer").val(nt);
+		entries = [];
+    temp = '';
+
+  } else {
+    entries.push(temp);
+    entries.push(val);
+    temp = '';
+  }
+});
